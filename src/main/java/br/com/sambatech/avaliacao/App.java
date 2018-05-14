ba9da18f150c43affc3b5364f39c54869c93522d
karinimagalhaes <karini.magalhaes@gmail.com>
2018-05-13 21:01:57 -0300

    Estutura do projeto maven

---
+++ src/main/java/br/com/sambatech/avaliacao/App.java
@@ -0,0 +1,13 @@
+package br.com.sambatech.avaliacao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
